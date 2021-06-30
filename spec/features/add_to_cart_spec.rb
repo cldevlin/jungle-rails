328370d9ded5676ef18ca92a809ff5248e10a181
Claire Devlin <claire.devlin@hotmail.com>
2021-06-29 22:08:18 -0400

    Add cart test

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature, js: true do
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,18 +16,16 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
     end
   end
 
-  scenario "They can navigate from home page to product detail page by clicking on product" do
+  scenario "When they click on Add, the cart count goes up by 1" do
     # ACT
     visit root_path
 
-    # click on product
-    find('article.product a', match: :first).click
-
+    # click on add to cart
+    find('article.product footer.actions button', match: :first).click
 
     # DEBUG / VERIFY
     save_screenshot
 
-    expect(page).to have_css 'section.products-show'
+    expect(find('#navbar ul > li.cart-link > a')).to have_content 'My Cart (1)'
   end
-
 end
